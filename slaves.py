bcfc35f099706c5b497ae2526de943d4ddad26eb
Denis Yuzhanin <cthulhuden@gmail.com>
2013-03-21 23:28:06 +0400

    Auto-detecting of cpu count

--- slaves.py
+++ slaves.py
@@ -2,21 +2,22 @@
 
 import multiprocessing
 
+def _func(function,operating,queueIn,queueOut,terminator,lock):
+	cont = True
+	while not(terminator.is_set()):
+		operating.wait()
+		lock.acquire()
+		if queueIn.empty():
+			lock.release()
+		else:
+			inp = queueIn.get()
+			lock.release()
+			rez = function(inp)
+			queueOut.put(rez)
+	queueOut.close()
+	return 0
+
 class slaves:
-	def func(self, function,operating,queueIn,queueOut,terminator,lock):		#move out of class definition if possible
-		cont = True
-		while not(terminator.is_set()):
-			operating.wait()
-			lock.acquire()
-			if queueIn.empty():
-				lock.release()
-			else:
-				inp = queueIn.get()
-				lock.release()
-				rez = function(inp)
-				queueOut.put(rez)
-		queueOut.close()
-		return 0
 	
 	def put(self,info):
 		self.queueIn.put(info)
@@ -35,7 +36,7 @@ class slaves:
 	def resume(self):
 		self.operating.set()
 
-	def __init__(self,number,function):
+	def __init__(self,function,number=multiprocessing.cpu_count()):
 		self.queueIn = multiprocessing.Queue()
 		self.queueOut = multiprocessing.Queue()
 		self.terminator = multiprocessing.Event()
@@ -43,7 +44,7 @@ class slaves:
 		self.lock = multiprocessing.Lock()
 		self.processes = []
 		for i in range(0,number):
-			self.processes.append(multiprocessing.Process(target=self.func,args=(function,self.operating,self.queueIn,self.queueOut,self.terminator,self.lock,)))
+			self.processes.append(multiprocessing.Process(target=_func,args=(function,self.operating,self.queueIn,self.queueOut,self.terminator,self.lock,)))
 
 	def start(self):
 		self.operating.set()
